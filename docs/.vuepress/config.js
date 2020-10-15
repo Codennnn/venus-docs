fc5abf24063db0577b7896b7fc3be54a542f12eb
陈梓聪 <chenzc@pvc123.com>
2020-10-15 14:04:34 +0800

    commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,22 @@
 module.exports = {
-  title: 'Hello VuePress1',
-  description: 'Just playing around',
-  theme: require.resolve('../../vuepress-theme-sonic/'),
   evergreen: true,
+  theme: require.resolve('../../vuepress-theme-sonic/'),
+  head: [
+    ['link', { rel: 'icon', href: '/logo/logo-dark.svg', media: '(prefers-color-scheme:light)' }],
+    ['link', { rel: 'icon', href: '/logo/logo-light.ico', media: '(prefers-color-scheme:dark)' }],
+    ['link', { rel: 'alternate icon', href: '/logo/logo-dark.ico', media: '(prefers-color-scheme:light)' }],
+    ['link', { rel: 'alternate icon', href: '/logo/logo-light.ico', media: '(prefers-color-scheme:dark)' }],
+    ['link', { rel: 'mask-icon', href: '/logo/logo-dark.svg', color: '#515151' }],
+  ],
   locales: {
     '/': {
       lang: 'zh-CN',
-      title: 'VuePress',
+      title: 'Sonic',
       description: 'Vue 驱动的静态网站生成器',
     },
     '/en/': {
       lang: 'en-US',
-      title: 'VuePress',
+      title: 'Sonic',
       description: 'Vue-powered Static Site Generator',
     },
   },
