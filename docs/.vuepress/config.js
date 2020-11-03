2468b212733029be1cbfb7dc52ba305dc9c1798b
令狐少侠 <czc12580520@gmail.com>
2020-11-03 22:50:18 +0800

    commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,6 +34,39 @@ const menus = [
   },
 ]
 
+
+function getNavbar() {
+  const nav = [
+    {
+      text: '文档',
+      link: '/docs/base/introduction',
+      items: menus.map(({ group, menu }) => ({
+        text: group,
+        items: menu.map(({ text, link }) => ({
+          text, link,
+        })),
+      })),
+    },
+    { text: '帮助', link: '/' },
+    { text: '关于', link: 'https://chinesee.gitee.io/blog-nuxt/' },
+  ]
+  return { nav }
+}
+
+function getSidebar() {
+  const sidebar = menus.map(({ group, menu }) => ({
+    title: group,
+    collapsable: false,
+    children: menu.map(({ link, text, external }) => {
+      if (external) {
+        return [link, text]
+      }
+      return link
+    }),
+  }))
+  return { sidebar }
+}
+
 module.exports = {
   evergreen: true,
   // theme: 'vuepress-theme-sonic',
@@ -71,36 +104,4 @@ module.exports = {
       // 修改客户端的 webpack 配置
     }
   },
-}
-
-function getNavbar() {
-  const nav = [
-    {
-      text: '文档',
-      link: '/docs/base/introduction',
-      items: menus.map(({ group, menu }) => ({
-        text: group,
-        items: menu.map(({ text, link }) => ({
-          text, link,
-        })),
-      })),
-    },
-    { text: '帮助', link: '/' },
-    { text: '关于', link: 'https://chinesee.gitee.io/blog-nuxt/' },
-  ]
-  return { nav }
-}
-
-function getSidebar() {
-  const sidebar = menus.map(({ group, menu }) => ({
-    title: group,
-    collapsable: false,
-    children: menu.map(({ link, text, external }) => {
-      if (external) {
-        return [link, text]
-      }
-      return link
-    }),
-  }))
-  return { sidebar }
 }
